44f0fa128ed2efd166360949625821d0d7fa1683
spencer <spencerwagner0@gmail.com>
2021-07-18 15:07:16 -0700

    added the db pages

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
-
+    
 @app.route('/')
 def main():
     return render_template('main.html')
-    
+
 @app.route('/index')
 def index():
     return render_template('index.html')   
