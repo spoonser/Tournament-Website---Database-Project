db67d07bd49995f3e1f069051c73934366ba1496
steelemeg <71856754+steelemeg@users.noreply.github.com>
2021-07-18 16:43:49 -0500

    Update app.py

--- app.py
+++ app.py
@@ -16,7 +16,6 @@ def fighters():
 @app.route('/weapons')
 def weapons():
     return render_template('weapons.html')
-    # TBD - might also allow a general search by weapon type, general search for all fighters using that weapon type
     
 @app.route('/results')
 def results():
@@ -25,12 +24,7 @@ def results():
 @app.route('/fights')
 def fights():
     return render_template('fights.html')
-    # placeholder -- will allow creation/modification of fights (who is participating, when it is)
-    # TBD - could also do the prizes-to-fights assignment and winner assignment here.
-    
-@app.route('/test')
-def hello_world():
-    return 'Hello, World!'
+
 	
 if __name__ == '__main__':
     app.run(host="localhost", port=61557, debug=True)
