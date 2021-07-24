e9a803af23b64c3ff821636ccb1669534fa73ef5
spencer <spencerwagner0@gmail.com>
2021-07-24 14:48:18 -0700

    CSS update

--- app.py
+++ app.py
@@ -2,13 +2,15 @@ from flask import Flask, render_template, json, redirect
 from flask_mysqldb import MySQL
 from flask import request
 
+import database.db_credentials as cred
+
 import os
 
 app = Flask(__name__)
-app.config['MYSQL_HOST'] = 'classmysql.engr.oregonstate.edu'
-app.config['MYSQL_USER'] = os.environ.get("CS340DBUSER")
-app.config['MYSQL_PASSWORD'] = os.environ.get("CS340DBPW")
-app.config['MYSQL_DB'] = os.environ.get("CS340DB")
+app.config['MYSQL_HOST'] = cred.host
+app.config['MYSQL_USER'] = cred.user
+app.config['MYSQL_PASSWORD'] = cred.passwd
+app.config['MYSQL_DB'] = cred.db
 app.config['MYSQL_CURSORCLASS'] = 'DictCursor'
 
 mysql = MySQL(app)
