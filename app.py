cb5d79b869566e247d49eac5a2f5dfbd5e2ce157
steelemeg <71856754+steelemeg@users.noreply.github.com>
2021-07-24 15:41:11 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,6 @@
 from flask import Flask, render_template, json, redirect
 from flask_mysqldb import MySQL
 from flask import request
-import database.db_connector as db
-import database.db_credentials as cred
 
 import os
 
