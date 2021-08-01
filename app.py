8818add0f1c439cb328366e2269f658b83f70dd3
spencer <spencerwagner0@gmail.com>
2021-07-31 17:53:20 -0700

    Created separate update pages for fighters, fights, weapons. UPDATE requests work, problem with single fight query

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, json, redirect
+from flask import Flask, render_template, json, redirect, url_for
 from flask_mysqldb import MySQL
 from flask import request
 
@@ -46,29 +46,57 @@ def modify_fighter():
                 VALUES (%s, %s);''', (fighterName, weapon))
             con.commit()
 
+            return fighters()
+
         except:
             print("Fighter Insert Failed")
     
     elif request.form.get('fighter-update'):
         fighterID = request.form.get('fighter-id') or None
-        con = mysql.connect
-        cur = con.cursor()
-        # If the fighterName or weapon wasn't provided, stay with the current values in the database.
-        cur.execute('''SELECT fighterName, weapon FROM Fighters WHERE fighterID = %s;''', (fighterID,))
         
-        fighterName = request.form.get('fighter-name') or cur.fetchone()['fighterName'] 
-        weapon = request.form.get('fighter-weapon') or cur.fetchone()['weapon']
- 
-        try:
-            
-            cur.execute('''UPDATE Fighters SET fighterName=%s, weapon=%s WHERE fighterID=%s;''', (fighterName, weapon, fighterID))
-            con.commit()
-
-        except:
-            print("Fighter Update Failed")
-            
-
+        if fighterID:
+            return redirect(url_for('.update_fighter_page', fighterID=fighterID))
+       
+        else:
+            print("No fighter ID entered")
+        
     return fighters()
+
+
+# Update a fighter
+@app.route('/update-fighter')
+def update_fighter_page():
+    fighterID = request.args['fighterID']
+    cur = mysql.connection.cursor()
+    cur.execute('''SELECT f.fighterName, f.fighterID, (IFNULL(w.weaponName, 'No Weapon')) as `weapon`  FROM Fighters as f
+        LEFT JOIN Weapons as w
+        on f.weapon=w.weaponID
+        WHERE fighterID=%s;''', (fighterID,))
+    fighters = cur.fetchall()
+
+    # Get the weapon names to populate the available Weapon choices when creating a new Fighter.
+    cur.execute('''SELECT weaponName, weaponID from Weapons''')
+    available_weapons = cur.fetchall()
+
+    return render_template('update/update-fighter.html', fighters=fighters, available_weapons=available_weapons)
+
+@app.route('/update-fighter', methods=['POST'])
+def update_fighter():
+    con = mysql.connect
+    cur = con.cursor()
+
+    fighterID = request.args.get('fighterID', default=1, type=int)
+    fighterName = request.form.get('fighter-name') or None
+    weapon = request.form.get('fighter-weapon') or None
+
+    try:
+        cur.execute('''UPDATE Fighters SET fighterName=%s, weapon=%s WHERE fighterID=%s;''', (fighterName, weapon, fighterID))
+        con.commit()
+
+    except:
+        print("Fighter Update Failed")
+
+    return redirect(url_for('.fighters'))
 
 # -------------------------------------------------------------------------------------------------
 # Weapons Page
@@ -104,20 +132,49 @@ def modify_weapon():
     
     elif request.form.get('weapon-update'):
         weaponID = request.form.get('weapon-id') or None
-        weaponName = request.form.get('weapon-name') or None
-        weaponType = request.form.get('weapon-type') or None
-        ranged = request.form.get('weapon-ranged')
-        try:
-            con = mysql.connection
-            cur = con.cursor()
-            cur.execute('''UPDATE Weapons SET weaponName=%s, weaponType=%s, ranged=%s WHERE weaponID=%s''', (weaponName, weaponType, ranged, weaponID))
-            con.commit()
-            
-        except:
-            print("Weapon Update Failed")
+
+        if weaponID:
+            return redirect(url_for('.update_weapon_page', weaponID=weaponID))
+
+        else:
+            print("No weapon selected")
 
     return weapons()
 
+
+# Update a weapon
+@app.route('/update-weapon')
+def update_weapon_page():
+    weaponID = request.args['weaponID']
+    cur = mysql.connection.cursor()
+
+    cur.execute('''SELECT w.weaponName, w.weaponID, w.weaponType, IF(w.ranged=1, "Yes", "No") as ranged,
+        IFNULL(GROUP_CONCAT(f.fighterName), "No Users") as `WeaponUsers`
+        FROM Weapons w
+        LEFT JOIN Fighters as f 
+        ON w.weaponID=f.weapon
+        WHERE w.weaponID=%s;''', (weaponID,))
+    weapons = cur.fetchall()
+
+    return render_template('update/update-weapon.html', weapons=weapons)
+    
+@app.route('/update-weapon', methods=['POST'])
+def update_weapon():
+    weaponID = request.args['weaponID']
+    weaponName = request.form.get('weapon-name') or None
+    weaponType = request.form.get('weapon-type') or None
+    ranged = request.form.get('weapon-ranged')
+
+    try:
+        con = mysql.connection
+        cur = con.cursor()
+        cur.execute('''UPDATE Weapons SET weaponName=%s, weaponType=%s, ranged=%s WHERE weaponID=%s''', (weaponName, weaponType, ranged, weaponID))
+        con.commit()
+        
+    except:
+        print("Weapon Update Failed")
+    
+    return redirect(url_for('.weapons'))
 
 # -------------------------------------------------------------------------------------------------
 # Results Page
@@ -214,34 +271,13 @@ def modify_fight():
     error=None
     print(request.form.to_dict())
     if request.form.get('fight-update'):
-        con = mysql.connection
-        cur = con.cursor()
-        fightID = request.form.get('old-fight-id') 
+        fightID = request.form.get('old-fight-id') or None
+
+        if fightID:
+            return redirect(url_for('.update_fight_page', fightID=fightID))
         
-        # If the fightDate and prizeWon weren't assigned, stay with the current values in the database.
-        cur.execute('''SELECT fightDate FROM Fights WHERE fightID = %s;''', (fightID,))
-        fightDate = request.form.get('new-fight-date') or cur.fetchone()['fightDate']
-        
-        cur.execute('''SELECT prize FROM Fights WHERE fightID = %s;''', (fightID,))
-        prizeID = request.form.get('new-prize-id') or cur.fetchone()['prize']
-        result = request.form.get('fight-winner') or None
-        fighter1Won = 0
-        fighter2Won = 0
-        
-        if result == 'fighter1-won': 
-            fighter1Won = 1
-            fighter2Won = 0
-        elif result == 'fighter2-won': 
-            fighter1Won = 0
-            fighter2Won = 1
-        print(fightID, fightDate, fighter1Won, fighter2Won, prizeID)
-        try:
-            cur.execute('''UPDATE Fights SET fightDate = %s, fighter1Won = %s, fighter2Won = %s, prize = %s WHERE fightID = %s;''', (fightDate, fighter1Won, fighter2Won, 
-            prizeID, fightID))
-            con.commit()
-        
-        except:
-             print('Fight Update Failed')
+        else:
+            print("Fight ID required for update")
         
     elif request.form.get('fight-insert'):
         fighter1 = request.form.get('fighter1-id') or None
@@ -302,6 +338,69 @@ def modify_fight():
         pass
     return fightsetup(error=error)
 
+
+# Update a fight
+@app.route('/update-fight')
+def update_fight_page(error=None):
+    fightID = request.args['fightID']
+    cur = mysql.connection.cursor()
+    cur.execute('''SELECT one.fightID, one.fightDate, one.fighter1, two.fighter2, IF(one.fighter1Won=1, one.fighter1, IF(one.fighter2Won=1, two.fighter2, "No Winner")) as winner,
+        IFNULL(Prizes.prizeType, "No Prize") as prize FROM
+        (SELECT Fights.fightID, Fights.fightDate, Fighters.fighterName as fighter1, Fights.fighter1Won, Fights.fighter2Won, Fights.prize
+        FROM Fights 
+        LEFT JOIN Fighters
+        on Fights.fighter1=Fighters.fighterID) as one
+        JOIN
+        (SELECT Fights.fightID, Fights.fightDate, Fighters.fighterName as fighter2
+        FROM Fights
+        LEFT JOIN Fighters
+        ON Fights.fighter2=Fighters.fighterID) AS two
+        LEFT JOIN Prizes 
+        ON one.prize=Prizes.prizeID
+        WHERE one.fightID=two.fightID;''')
+    fights = cur.fetchall()
+    cur.execute('''SELECT fightID from Fights ORDER BY fightID asc;''')
+    fightIDs = cur.fetchall()
+    cur.execute('''SELECT fighterName, fighterID from Fighters ORDER BY fighterName asc;''')
+    available_fighters=cur.fetchall()
+    cur.execute('''SELECT prizeID, prizeType from Prizes;''')
+    available_prizes=cur.fetchall()
+    return render_template('update/update-fight.html', fights=fights, available_fighters=available_fighters, available_prizes=available_prizes, fightIDs=fightIDs, error=error)
+
+@app.route('/update-fight', methods=['POST'])
+def update_fight():
+    fightID = request.args['fightID']
+    con = mysql.connection
+    cur = con.cursor()
+
+    # If the fightDate and prizeWon weren't assigned, stay with the current values in the database.
+    cur.execute('''SELECT fightDate FROM Fights WHERE fightID = %s;''', (fightID,))
+    fightDate = request.form.get('new-fight-date') or cur.fetchone()['fightDate']
+        
+    cur.execute('''SELECT prize FROM Fights WHERE fightID = %s;''', (fightID,))
+    prizeID = request.form.get('new-prize-id') or cur.fetchone()['prize']
+    result = request.form.get('fight-winner') or None
+    fighter1Won = 0
+    fighter2Won = 0
+        
+    if result == 'fighter1-won': 
+        fighter1Won = 1
+        fighter2Won = 0
+
+    elif result == 'fighter2-won': 
+        fighter1Won = 0
+        fighter2Won = 1
+
+    try:
+        cur.execute('''UPDATE Fights SET fightDate = %s, fighter1Won = %s, fighter2Won = %s, prize = %s WHERE fightID = %s;''', (fightDate, fighter1Won, fighter2Won, 
+        prizeID, fightID))
+        con.commit()
+        
+    except:
+        print('Fight Update Failed')
+
+    return redirect(url_for('.fightsetup'))
+
 # -------------------------------------------------------------------------------------------------
 # Prizes Page
 @app.route('/prizes')
@@ -340,7 +439,7 @@ def modify_prize():
             print("Prizes - Insert Failed")
     elif request.form.get('prize-won'):
         prizeID = request.form.get('prize-id') 
-        fighterID = request.form.get('fighter-id') 
+        fighterID = request.form.get('fighter-id')
         try:
             con = mysql.connection
             
